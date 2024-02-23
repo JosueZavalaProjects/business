05faab6a23ddfde2038e532b88e689abee10faae
Josue Zavala <josue.zavala.projects@gmail.com>
2024-02-23 15:26:10 -0600

    Cleaning code

--- components/modules/inventory/index.tsx
+++ components/modules/inventory/index.tsx
@@ -11,37 +11,8 @@ import { BackArrow, CameraIcon } from "@/public/assets";
 import { NumberButton } from "./numbers";
 
 export const Inventory = () => {
-  /* const [amount, setAmount] = useState<string>(""); */
   const [unit, setUnit] = useState<"pzs" | "kg" | "lt">("kg");
   const { amount, handleSetAmount } = useAmount(unit);
-
-  /* const handleSetAmount = (
-    character: number | string,
-    unit: "pzs" | "kg" | "lt" = "pzs"
-  ) => {
-    if (character === "<") {
-      const newAmount = amount.split("", amount.length - 1).join("");
-
-      setAmount(newAmount !== "0" ? newAmount : "");
-      return;
-    }
-    if (character === ".") {
-      if (unit === "pzs") return;
-
-      const newAmount = amount.split(".");
-
-      if (!newAmount[0]) {
-        setAmount("0.");
-        return;
-      }
-      if (newAmount.length > 1) return;
-    }
-    const decimalPart = amount.split(".");
-    if (decimalPart.length > 1 && decimalPart[1].length > 2) return;
-
-    const newAmount = amount + `${character}`;
-    setAmount(newAmount !== "0" ? newAmount : "");
-  }; */
 
   return (
     <div className="grid gap-4 bg-main-blue p-4">
