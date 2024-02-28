c29379faefa9074678329bc994ac2c424d93c3e3
Josue Zavala <josue.zavala.projects@gmail.com>
2024-02-27 22:01:41 -0600

    Correcting useAmount

--- components/modules/inventory/index.tsx
+++ components/modules/inventory/index.tsx
@@ -1,107 +1,29 @@
 "use client";
 import { useState } from "react";
 
-import Image from "next/image";
-
-import { Button } from "@/components/UI/button";
-import Text from "@/components/UI/text";
 import { useAmount } from "@/hook/useAmount";
-import { BackArrow, CameraIcon } from "@/public/assets";
-import { Unit, UnitsObject } from "@/types/inventory";
+import { Unit } from "@/types/inventory";
 
-import { NumberButton } from "./numbers";
+import { SetUnits } from "./set-units";
 
 export const Inventory = (): React.ReactElement => {
+  const [step, setStep] = useState<number>(0);
   const [unit, setUnit] = useState<Unit>("pzs");
   const { amount, handleSetAmount, removeDecimalPart } = useAmount(unit);
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setUnit(UnitsObject[e.target.value]);
-    removeDecimalPart(UnitsObject[e.target.value]);
-  };
-
   return (
-    <div className="grid gap-4 bg-main-blue p-4">
-      <div className="grid justify-items-center items-center">
-        <Text color="white" size="4xl" as="h1">
-          Inventario
-        </Text>
-      </div>
-      <div className="flex justify-between px-4">
-        <div className="flex gap-4">
-          <Image
-            src={BackArrow}
-            width={15}
-            height={26}
-            alt="back arrow"
-            className="cursor-pointer"
-          />
-          <div className="flex flex-col">
-            <Text color="white" size="sm">
-              Balance
-            </Text>
-            <div className="flex items-end gap-2">
-              <Text color="white" size="2xl">
-                704.00
-              </Text>
-              <Text color="white" size="sm">
-                MXN
-              </Text>
-            </div>
-          </div>
-        </div>
-        <Image
-          src={CameraIcon}
-          width={38}
-          height={31}
-          alt="camara icon"
-          className="cursor-pointer"
+    <>
+      {step === 0 && (
+        <SetUnits
+          amount={amount}
+          unit={unit}
+          setStep={setStep}
+          handleSetAmount={handleSetAmount}
+          removeDecimalPart={removeDecimalPart}
+          setUnit={setUnit}
         />
-      </div>
-      <div className="grid gap-4 justify-items-center items-cener py-4">
-        <div className="flex flex-col w-full items-center">
-          <div className="flex gap-2 w-2/3 justify-center items-end py-2">
-            <Text
-              color="white"
-              size="5xl"
-              className="max-w-40 overflow-x-scroll overflow-y-hidden justify-start px-4"
-            >
-              {amount ? amount : 0}
-            </Text>
-            <Text color="white" size="sm" className="capitalize w-1/12">
-              {unit}
-            </Text>
-          </div>
-          <div className="flex w-full justify-center">
-            <select
-              name="selectedFruit"
-              onChange={(e) => {
-                handleChange(e);
-              }}
-              className="text-white p-2 w-4/12 rounded-3xl border border-white bg-transparent focus:outline-none"
-            >
-              <option value={UnitsObject.pzs}>Pzas</option>
-              <option value={UnitsObject.kg}>Kg</option>
-              <option value={UnitsObject.lt}>Lt</option>
-            </select>
-          </div>
-        </div>
-        <div className="grid grid-cols-3 w-full gap-4 px-4">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((number, index) => (
-            <NumberButton
-              key={`NumberButton_${index}`}
-              handleSetAmount={handleSetAmount}
-              character={number}
-            />
-          ))}
-          <NumberButton handleSetAmount={handleSetAmount} character={"."} />
-          <NumberButton handleSetAmount={handleSetAmount} character={0} />
-          <NumberButton handleSetAmount={handleSetAmount} character={"<"} />
-        </div>
-        <div className="grid gap-4 justify-items-center items-cener py-4">
-          <Button disabled={Boolean(!amount)}>Agregar</Button>
-        </div>
-      </div>
-    </div>
+      )}
+      {step === 1 && <div>paso 2</div>}
+    </>
   );
 };
--- hook/useAmount.tsx
+++ hook/useAmount.tsx
@@ -5,26 +5,43 @@ import { Unit } from "@/types/inventory";
 export const useAmount = (unit: Unit) => {
   const [amount, setAmount] = useState<string>("");
 
+  const _removeCharacter = () => {
+    const newAmount = amount.split("", amount.length - 1).join("");
+
+    setAmount(newAmount !== "0" ? newAmount : "");
+  };
+
+  const _decimalPoint = () => {
+    const newAmount = amount.split(".");
+
+    if (unit === "pzs") return false;
+
+    if (!newAmount[0]) {
+      setAmount("0.");
+      return false;
+    }
+    if (newAmount.length > 1) return false;
+
+    return true;
+  };
+
+  const _checkDecimalPart = () => {
+    const newAmount = amount.split(".");
+    if (newAmount.length > 1 && newAmount[1].length > 2) return false;
+
+    return true;
+  };
+
   const handleSetAmount = (character: number | string) => {
     if (character === "<") {
-      const newAmount = amount.split("", amount.length - 1).join("");
-
-      setAmount(newAmount !== "0" ? newAmount : "");
+      _removeCharacter();
       return;
     }
     if (character === ".") {
-      if (unit === "pzs") return;
-
-      const newAmount = amount.split(".");
-
-      if (!newAmount[0]) {
-        setAmount("0.");
-        return;
-      }
-      if (newAmount.length > 1) return;
+      const isValidPoint = _decimalPoint();
+      if (!isValidPoint) return;
     }
-    const decimalPart = amount.split(".");
-    if (decimalPart.length > 1 && decimalPart[1].length > 2) return;
+    if (!_checkDecimalPart()) return;
 
     const newAmount = amount + `${character}`;
     setAmount(newAmount !== "0" ? newAmount : "");
